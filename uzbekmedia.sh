ee42f5f0a0fab31b0a331d670b1926449640b7fd
Parsast <parsastdev@duck.com>
2026-07-05 13:54:35 +0330

    fix: ai exclude

--- uzbekmedia.sh
+++ uzbekmedia.sh
@@ -13,7 +13,7 @@ HARDCODED_RULE34_EXCLUDED_TAG=+-ai_generated
 YANDERE_URL="https://yande.re"
 KONACHAN_URL="https://konachan.com"
 DANBOORU_API="https://danbooru.donmai.us/posts.json?tags="
-RULE34_API="https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1"
+RULE34_API="https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags="
 RULE34_API_KEY="1b7ac421841879b859d0a0d771b6df8e4a3419e0ce54f234e90bc0c8969ac6ada2972a757a25a606e33dfa9bbd946a0ca231f90c38644336367db73f918b61ed"
 RULE34_USER_ID="5925125"
 API_QUERY="/post.json?tags="
@@ -55,7 +55,7 @@ case "$BOORU" in
     JQ_FILTER='.[].file_url'
     ;;
   rule34)
-    URL="${RULE34_API}${HARDCODED_RULE34_EXCLUDED_TAG}&tags=${TAG}&api_key=${RULE34_API_KEY}&user_id=${RULE34_USER_ID}"
+    URL="${RULE34_API}${TAG}${HARDCODED_RULE34_EXCLUDED_TAG}&api_key=${RULE34_API_KEY}&user_id=${RULE34_USER_ID}"
     DIR="rule34_$TAGS"
     JQ_FILTER='.[].file_url'
     ;;
